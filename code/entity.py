1b5f98c1ddf4f71d2a152eb8dfcb2df12c257d1c
Sara michalach <sara_michalach@outlook.com>
2025-02-21 10:04:52 -0300

    Adding classes generated from StarUml

---
+++ code/entity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Interface2 import Interface2
+
+
+class Entity(Interface2):
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
---
+++ code/entityFactory.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Interface8 import Interface8
+
+
+class EntityFactory(Interface8):
+    def __init__(self):
+        pass
+
+    def get_entity(self, entity_type):
+        pass
